d8100c67ebdb11aadf867a77004edaf5f4a5f627
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-05-05 09:20:16 +0530

    Update ReadMe

--- src/layouts/NotFound.js
+++ src/layouts/NotFound.js
@@ -1,10 +1,11 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
+import '../styles/NotFound.css';
 
 const NotFound = () => (
   <div>
     <NavBar />
-    Page not Found
+    <img src="https://culapp.co/static/media/notFound.914aef37.png" alt="Page Not Found" className="errorPage" />
     <Footer />
   </div>
 );
